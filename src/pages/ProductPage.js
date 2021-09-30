b2b852cd09320f37c71b8e89a312a84bfdec8528
Szymon Mrzyglod <szymon.mr@gmail.com>
2021-09-30 09:08:12 +0200

    create ProductPage

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,10 +1,15 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import Product from "../components/Product";
 
-const ProductPage = () => {
+const ProductPage = ({match}) => {
     return ( 
-    <div>
-        Product Page
-    </div> );
+        <>
+            <div>Strona Produktu</div> 
+            <Product id={match.params.id}/>
+            <Link to ="/products">Powrót do listy produktów</Link>
+        </>
+    );
 }
  
 export default ProductPage;
