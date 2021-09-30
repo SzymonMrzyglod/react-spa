0070f9a366bda663250b9838b68de954c31e5d4a
Szymon Mrzyglod <szymon.mr@gmail.com>
2021-09-30 08:07:12 +0200

    create AdminPage

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -1,9 +1,14 @@
 import React from "react";
+import {Route, Redirect} from 'react-router-dom';
+
+const permission = false;
 
 const AdminPage = () => {
     return ( 
     <div>
-        Product Page
+        <Route render={() => (
+            permission ? (<h3>Panel admina - dzień dobry</h3>):(<Redirect to='/login'/>)
+        )}/>
     </div> );
 }
  
